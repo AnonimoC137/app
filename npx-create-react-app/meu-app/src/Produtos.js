3bbec5d07f5837d88d1b4fd3292d650da7acf889
AnonimoC137 <alexandrecoimbra44@gmail.com>
2023-02-15 21:08:49 -0300

    att

--- npx-create-react-app/meu-app/src/Produtos.js
+++ npx-create-react-app/meu-app/src/Produtos.js
@@ -2,9 +2,18 @@ import React from 'react'
 import Titulo from './Titulo'
 
 const Produtos = () => {
+    const produtos = [
+        {nome: 'Notbook', propriedades: ['16gb ram', '512gb']},
+        {nome: 'Smartphone', propriedades: ['2gb ram', '128gb']}
+    ]
+
+    const dados = produtos.map(({nome}) => (
+    <li>{nome}</li>
+    ))
   return (
     <section>
       <Titulo texto='Produtos'/>
+      {dados}
     </section>
   )
 }
