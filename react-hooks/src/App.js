bf217097c5f8566d86b53fb7a3fcf1031cba7c2b
AnonimoC137 <alexandrecoimbra44@gmail.com>
2023-03-28 18:58:33 -0300

    att

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import useLocalStorage from './useLocalStorage'
 
 
 
 const App = () => {
-  const [produto, setProduto] = useLocalStorage('produto', 'caneta')
-  function handleClick({target}) {
-    setProduto(target.innerText);
+  
     
-  }
   return (
     <div>
-      <button onClick={handleClick}>notebook</button>
-      <button onClick={handleClick}>smartphone</button>
+     <a href="https://www.origamid.com" title="Site Origamid">
+        Origamid
+        </a>
     </div>
-  ) 
-      
+  )      
 }
 
 export default App;
