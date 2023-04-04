a80b405eca70c7d74039fbff7033b199806e7465
AnonimoC137 <alexandrecoimbra44@gmail.com>
2023-04-04 17:31:22 -0300

    att

--- formularios/src/Hooks/useForm.js
+++ formularios/src/Hooks/useForm.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
+const types = {
+  cep: {
+    regex: /^\d{5}-?\d{3}$/,
+    messagem: "Cep Invalido"
+  }
 
+}
 
-const useForm = () => {
+const useForm = (type) => {
+  const [value, setValue] = React.useState(' ');
+  const [erro, setErro] = React.useState(null);
+
+  function validaCep(value) {
+    if(value.length === 0) {
+       setErro('Preeencha um valor');
+       return false;
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+       setErro('preencha um CEP valido');
+       return false;
+
+    } else {
+       setErro(null)
+       return true;
+    }
+ }
+
   return (
     <div>
       
