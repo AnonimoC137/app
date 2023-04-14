2fe66d510ac1170d09e447e02235d35788f50786
AnonimoC137 <alexandrecoimbra44@gmail.com>
2023-04-14 13:56:50 -0300

    criando um slide com css e react

--- css/src/Slide.js
+++ css/src/Slide.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import styles from "./Slide.module.css"
 
 const Slide = ({slides}) => {
   return (
-    <div>
-      {slides.map((slide) => (
-        <div key={slide.id}>{slide.texto}</div>
-      ))}
-    </div>
+    <section className={styles.container}>
+        <div className={styles.content}>
+                {slides.map((slide) => (
+                <div key={slide.id} className={styles.item}>{slide.texto}</div>
+            ))}
+        </div>
+        <nav className={styles.nav}>
+            <button>Anterior</button>
+            <button>Proximo</button>
+        </nav>
+      
+    </section>
   )
 }
 
