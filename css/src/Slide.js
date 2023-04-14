413f40d7932e6c4ee6caf39285f0ff7d0dd790e8
AnonimoC137 <alexandrecoimbra44@gmail.com>
2023-04-14 15:24:01 -0300

    att

--- css/src/Slide.js
+++ css/src/Slide.js
@@ -2,17 +2,36 @@ import React from 'react'
 import styles from "./Slide.module.css"
 
 const Slide = ({slides}) => {
-    const [active, setActive] = React.useState(0)
+    const [active, setActive] = React.useState(0);
+    const [position, setPosition] = React.useState(0);
+    const contentRef = React.useRef();
+
+    React.useEffect(() => {
+        const {width} = contentRef.current.getBoundingClientRect()
+        setPosition( -(width * active))
+    }, [active])
+
+    function slidePrev() {
+        if (active > 0 ) setActive(active - 1)
+    }
+
+    function slideNext() {
+        if(active < slides.length - 1) setActive(active + 1)
+    }
+
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
+        <div
+        ref={contentRef} 
+        className={styles.content} 
+        style={{transform: `translateX(${position}px)`}}>
                 {slides.map((slide) => (
                 <div key={slide.id} className={styles.item}>{slide.texto}</div>
             ))}
         </div>
         <nav className={styles.nav}>
-            <button>Anterior</button>
-            <button>Proximo</button>
+            <button onClick={slidePrev}>Anterior</button>
+            <button onClick={slideNext}>Proximo</button>
         </nav>
       
     </section>
