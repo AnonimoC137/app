f7b76af72719027d81d8af444b574e09ba3d850f
AnonimoC137 <alexandrecoimbra44@gmail.com>
2023-05-03 15:54:55 -0300

    att

--- teste/src/Componentes/Main.js
+++ teste/src/Componentes/Main.js
@@ -16,6 +16,7 @@ const personagens = [
     texto: 'O naruto quando nasceu rescebeu o espirito da raposa demonia de nova causas.',
     texto2: 'Naruto ganha um manto protetor da raposa de nove caudas, aumentando muito seu poder e sua velocidade',
     id: 1,
+    id2: 'a',
     punho: '../img/punho2.png',
     forca: 50,
     raio: '../img/raio4.png',
@@ -30,6 +31,7 @@ const personagens = [
     texto: 'O sasuke é o ultimo sobrevivente do massacre ao clã uchiha além de seu irmão itachi.',
     texto2: 'Sasuke obtem essa transformação após ser amaldiçoado por Orochimaru',
     id: 2,
+    id2: 'b',
     punho: '../img/punho2.png',
     forca: 40,
     raio: '../img/raio4.png',
@@ -44,6 +46,7 @@ const personagens = [
     texto: 'Kakashi é um ninja de elite, possui o sharingan e tem mais de 1000 jutsus',
     texto2: 'quando Kakashi ativa seu mangekyo seus poderem aumentam muito, porem gasta seu chakra muito rapido',
     id: 2,
+    id2: 'c',
     punho: '../img/punho2.png',
     forca: 60,
     raio: '../img/raio4.png',
@@ -56,33 +59,50 @@ const personagens = [
 
 const Main = () => {
   const [mudar, setMudar] = React.useState(personagens);
-  const [ativa, setAtiva] = React.useState(true)
+  const [ativa, setAtiva] = React.useState(false)
   
-  function handleClick() {
-    setAtiva(!true)
-    if(ativa){
+  function handleClick({id}) {
+    setAtiva(true)
+    if (ativa)
+    if(id === 1){
+      setAtiva(!true)
       setMudar(
         mudar[0].img = mudar[0].img2,
-        mudar[1].img = mudar[1].img2,
-        mudar[2].img = mudar[2].img2
-      )
-      setMudar(
         mudar[0].forca = 100,
-        mudar[1].forca = 110  
+        mudar[0].texto =  mudar[0].texto2
       )
+      setMudar(personagens)
+      
+
+    } else if(id === 2){
+      setAtiva(!true)
       setMudar(
-        mudar[0].texto =  mudar[0].texto2,
-        mudar[1].texto = mudar[1].texto2,
-        mudar[2].texto = mudar[2].texto2
+        mudar[1].img = mudar[1].img2,
+        mudar[1].forca = 100,
+        mudar[1].texto =  mudar[1].texto2
       )
+      setMudar(personagens)
+      
+    } else if(id === 3) {
+      setAtiva(!true)
+      setMudar(
+        mudar[2].img = mudar[2].img2,
+        mudar[2].forca = 100,
+        mudar[2].texto =  mudar[2].texto2
+      )
+      setMudar(personagens)
+      
+      
     }
+
+    console.log(personagens)
     
      
   }
   return (
     <div className={styles.container}>
         <h1 className={styles.tituloPrincipal}>A historia de cada personagem de Naruto</h1>
-        {personagens.map(({nome, texto, img, id, punho, forca, raio,velocidade,escudo,defesa }) => (
+        {personagens.map(({nome, texto, img, id, punho, forca, raio,velocidade,escudo,defesa, id2 }) => (
             <div className={styles.content} key={id}>
               <h1 className={styles.nomePersonagem}>{nome}</h1>
               <div className={styles.campoImg}>
@@ -100,7 +120,8 @@ const Main = () => {
                   
               </div>
               <button
-                      className={styles.buttonTransformar}onClick=   {handleClick}>Transformar Todos 
+                      
+                      className={styles.buttonTransformar}onClick=   {() => handleClick({id})}>Transformar Todos 
                       <BallTriangle
                                 height={50}
                                 width={50}
