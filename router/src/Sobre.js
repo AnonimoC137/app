6ae61ff67a278d416689eeeedd264977919f6302
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-11 20:09:00 -0300

    att

--- router/src/Sobre.js
+++ router/src/Sobre.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Head from './Head'
 
 const Sobre = () => {
   return (
     <div>
+      <Head title='Sobre'/>
       <h1>Sobre</h1>
       <p>Essa é a pagina sobre</p>
     </div>
