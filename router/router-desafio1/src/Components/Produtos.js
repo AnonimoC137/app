366102c77848e911351618dd970d7b599f6e047c
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-15 19:27:27 -0300

    att

--- router/router-desafio1/src/Components/Produtos.js
+++ router/router-desafio1/src/Components/Produtos.js
@@ -2,9 +2,24 @@ import React from 'react'
 import styles from './Produtos.module.css'
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+    .then(response => response.json())
+    .then(json => setProdutos(json))
+  },[])
+
+  if(produtos === null) return null
+
   return (
     <div className={styles.produtos}>
-      <h1>Produtos</h1>
+      {produtos.map((produto) => (
+        <div key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].src} />
+          <h1>{produto.nome}</h1>
+        </div>
+      ))}
     </div>
   )
 }
