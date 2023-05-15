b893c25e077d9d9eeb6451b46d69501f8b5e4c01
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-15 19:16:27 -0300

    att

--- router/router-desafio1/src/Components/Contato.js
+++ router/router-desafio1/src/Components/Contato.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import styles from './Contato.module.css'
 import foto from '../img/contato.jpg'
+import Head from './Head'
 
 const Contato = () => {
+    <Head/>
   
   return (
-    <section classname="{styles.contato}">
-
-      <h1>Contato</h1>
+    <section className={`${styles.contato} animeLeft`}>
+      <Head title='Contato' description='Entre em contato'/>
+      <img src={foto} alt="maquina de escrever"/>
+      <div>
+        <h1>Contato</h1>
+        <ul className={styles.dados}>
+          <li>coimbra.dev@gmail.com</li>
+          <li>(047)984129911</li>
+          <li>rua maria delfina de castro 234</li>
+        </ul>
+      </div>
+      
     </section>
   )
 }
---
+++ router/router-desafio1/src/Components/Head.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Head = (props) => {
+    React.useEffect(() => {
+        document.title = props.title
+        document
+        .querySelector('meta[name="description"]')
+        .setAttribute('content', props.title)
+    },[props])
+
+  return (
+    <></>
+  )
+}
+
+export default Head
