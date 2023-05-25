081f232a01918272e2eb8a2c932b7f63f4737d23
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-25 11:56:29 -0300

    aTT

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import Api from './api/Api';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
--- dogs/src/Components/Header.js
+++ dogs/src/Components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.header}>
+      <nav className={styles.container}>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Criar</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
