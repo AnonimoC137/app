90f604c5af71a993bc66eae373138194633d5e09
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-25 10:11:59 -0300

    att

--- dogs/src/api/endpoints/PhotoPost.js
+++ dogs/src/api/endpoints/PhotoPost.js
@@ -1,7 +1,61 @@
 import React from 'react';
 
 const PhotoPost = () => {
-  return <div></div>;
+  const [token, setToken] = React.useState('');
+  const [nome, setNome] = React.useState('');
+  const [peso, setPeso] = React.useState('');
+  const [idade, setIdade] = React.useState('');
+  const [img, setImg] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://dogsapi.origamid.dev/json/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {},
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        return json;
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="token"
+        value={token}
+        onChange={({ target }) => setToken(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="nome"
+        value={nome}
+        onChange={({ target }) => setNome(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="peso"
+        value={peso}
+        onChange={({ target }) => setPeso(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="idade"
+        value={idade}
+        onChange={({ target }) => setIdade(target.value)}
+      />
+      <input type="file" onChange={({ target }) => setImg(target.value)} />
+      <button>Enviar</button>
+    </form>
+  );
 };
 
 export default PhotoPost;
