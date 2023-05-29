913cb24728a5b21c4a4462f9c6d6bffd14a66a15
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-29 11:30:11 -0300

    att

--- dogs/src/Components/Login/Login.js
+++ dogs/src/Components/Login/Login.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import LoginForm from './LoginForm';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
+import LoginCreate from './LoginCreate';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
---
+++ dogs/src/Components/Login/LoginForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  return (
+    <section>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastros</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
