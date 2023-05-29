6889902c0ed40dcb34b912d1d1549db27cb40a7c
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-29 12:29:26 -0300

    att

--- dogs/src/Components/Forms/Button.js
+++ dogs/src/Components/Forms/Button.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-const Button = ({ children }) => {
-  return <button className={styles.button}>{children}</button>;
+const Button = ({ children, ...props }) => {
+  return (
+    <button {...props} className={styles.button}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- dogs/src/Components/Forms/Input.js
+++ dogs/src/Components/Forms/Input.js
@@ -3,7 +3,7 @@ import styles from './Input.module.css';
 
 const Input = ({ label, type, name }) => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <label htmlFor={name}>{label}</label>
       <input id={name} name={name} className={styles.input} type={type} />
     </div>
