b4c352b1ce889614a1b3fb5ab871be48cc5d98cd
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-05-29 12:19:32 -0300

    att

---
+++ dogs/src/Components/Forms/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+const Input = ({ label, type, name }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input id={name} name={name} className={styles.input} type={type} />
+    </div>
+  );
+};
+
+export default Input;
