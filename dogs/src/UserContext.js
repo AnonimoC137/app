91160d7ecb6e971022e89599adce689cb577bede
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-06-01 19:45:24 -0300

    att

--- dogs/src/UserContext.js
+++ dogs/src/UserContext.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TOKEN_POST, USER_GET } from './api';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
 
 export const UserContext = React.createContext();
 
@@ -8,6 +8,20 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token');
+      if (token) {
+        const { url, options } = TOKEN_VALIDATE_POST(token);
+        const response = await fetch(url, options);
+        if (!response.ok) throw new Error('Token invalido');
+        const json = response.json();
+        console.log(json);
+      }
+    }
+    autoLogin();
+  }, []);
 
   /*para puxar o usuario*/
   async function getUser(token) {
