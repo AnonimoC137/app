5864b7e880ca56417e7bd7f12f8b1c45a7227405
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2024-06-26 09:37:23 -0300

    att

--- dogs/src/Components/User/UserHeader.js
+++ dogs/src/Components/User/UserHeader.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import UserHeaderNav from './UserHeaderNav';
+import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router';
 
 const UserHeader = () => {
+  const [titulo, setTitulo] = React.useState('');
+  const localiza = useLocation();
+
+  React.useEffect(() => {
+    const { pathname } = localiza;
+
+    switch (pathname) {
+      case '/conta/postar':
+        setTitulo('Poste sua foto');
+        break;
+      case '/conta/estatisticas':
+        setTitulo('Estatísticas');
+        break;
+      default:
+        setTitulo('Minha Conta');
+    }
+  }, [localiza]);
+
   return (
-    <header>
-      <h1 className="title">Titulo</h1>
+    <header className={styles.header}>
+      <h1 className="title">{titulo}</h1>
       <UserHeaderNav />
     </header>
   );
