1170f8273167ae4e572d9d31d234ba4cf9f3212e
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2024-06-27 08:30:07 -0300

    att

--- dogs/src/Components/User/UserHeaderNav.js
+++ dogs/src/Components/User/UserHeaderNav.js
@@ -6,56 +6,74 @@ import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
 import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
 import { ReactComponent as Sair } from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import useMedia from '../../Hooks/useMedia';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia('( max-width: 40rem )');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
   return (
-    <nav className={styles.nav}>
-      <NavLink
-        to="/conta"
-        end
-        style={({ isActive }) => ({
-          background: isActive ? 'white' : '',
-          boxShadow: isActive ? '0 0 0 3px #fea' : '',
-          borderColor: isActive ? '#fb1' : '',
-          color: isActive ? '#fb1' : 'black',
-        })}
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          } `}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
       >
-        <MinhasFotos />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink
-        to="/conta/estatisticas"
-        style={({ isActive }) => ({
-          background: isActive ? 'white' : '',
-          boxShadow: isActive ? '0 0 0 3px #fea' : '',
-          borderColor: isActive ? '#fb1' : '',
-          color: isActive ? '#fb1' : 'black',
-        })}
-      >
-        <Estatisticas />
-        {mobile && 'Estatisticas'}
-      </NavLink>
-      <NavLink
-        to="/conta/postar"
-        style={({ isActive }) => ({
-          background: isActive ? 'white' : '',
-          boxShadow: isActive ? '0 0 0 3px #fea' : '',
-          borderColor: isActive ? '#fb1' : '',
-          color: isActive ? '#fb1' : 'black',
-        })}
-      >
-        <AdicionarFoto />
-        {mobile && 'Adicionar Foto'}
-      </NavLink>
-      <button onClick={userLogout}>
-        {''}
-        <Sair />
-        {mobile && 'Sair'}
-        Sair
-      </button>
-    </nav>
+        <NavLink
+          to="/conta"
+          end
+          style={({ isActive }) => ({
+            background: isActive ? 'white' : '',
+            boxShadow: isActive ? '0 0 0 3px #fea' : '',
+            borderColor: isActive ? '#fb1' : '',
+            color: isActive ? '#fb1' : 'black',
+          })}
+        >
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink
+          to="/conta/estatisticas"
+          style={({ isActive }) => ({
+            background: isActive ? 'white' : '',
+            boxShadow: isActive ? '0 0 0 3px #fea' : '',
+            borderColor: isActive ? '#fb1' : '',
+            color: isActive ? '#fb1' : 'black',
+          })}
+        >
+          <Estatisticas />
+          {mobile && 'Estatisticas'}
+        </NavLink>
+        <NavLink
+          to="/conta/postar"
+          style={({ isActive }) => ({
+            background: isActive ? 'white' : '',
+            boxShadow: isActive ? '0 0 0 3px #fea' : '',
+            borderColor: isActive ? '#fb1' : '',
+            color: isActive ? '#fb1' : 'black',
+          })}
+        >
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={userLogout}>
+          {''}
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 };
 
