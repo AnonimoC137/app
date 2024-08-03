477a700795a6a613a3b59dc6f4653015fb10254d
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2024-08-03 09:15:08 -0300

    att

--- dogs/src/Components/Photo/PhotoContent.js
+++ dogs/src/Components/Photo/PhotoContent.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styles from './PhotoContent.module.css';
+import { Link } from 'react-router-dom';
 
-const PhotoContent = ({data}) => {
-  return <div className={styles.photo}>
-      <div>
-</div>
-  </div>;
+const PhotoContent = ({ data }) => {
+  const { photo, comments } = data;
+
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.atributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} kg</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoContent;
