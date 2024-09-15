f6cf11a8f8f56920ce9de722cdf741402fcb4ba8
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2024-09-15 11:44:38 -0300

    criação da page de esqueci a senha e page 404

--- dogs/src/Components/Login/LoginPasswordLost.js
+++ dogs/src/Components/Login/LoginPasswordLost.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../api';
+import Error from '../Helper/Error';
 
 const LoginPasswordLost = () => {
-  return <div></div>;
+  const login = useForm();
+  const {data, loading, error, request} = useFetch();
+
+  async function handleSubmit(e) {
+      e.preventDefault();
+
+      if(login.validate()) {
+          const { url, options } = PASSWORD_LOST({
+            login: login.value,
+            url: window.location.href.replace('perdeu', 'resetar'),
+          });
+          await request(url, options);
+      }
+      
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a sua senha?</h1>
+      {data ? (
+        <p style={{color: '#4c1'}}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="email" {...login} />
+          {loading ? (
+            <Button disabled>Enviando</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+
+      {error && <Error error={error} />}
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
